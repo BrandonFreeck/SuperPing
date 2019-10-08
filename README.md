4bfd052f3bd154c2e65f57c7b3c4f52b7b4e26b7
brandonfreeck <brandonfreeck@gmail.com>
2019-10-07 21:07:13 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SuperPing
+PowerShell script that Pings X IP addresses/hostnames given as an argument, for Y hours given as an argument, and logs the results to a .csv for each destination. Will handle as many arguments as given, simultaneously, using PowerShell jobs functionality to run them side by side.
---
+++ SuperPing/SuperPingWithIPV4.ps1
@@ -0,0 +1,49 @@
+﻿Param (
+    [array]$Targets = ("google.com", "192.168.7.1", "8.8.8.8", "wikipedia.org"),
+    $Hours = .01
+    )
+
+$Code = {
+    Param (
+    $Duration = .01,
+    $TestTarget = "google.com",
+    $LogPath = $PSScriptRoot + "\log.csv"
+    )
+
+    $Count = $Duration*60*60
+    $Ping = @()
+
+    #Test if path exists, if not, create it
+    If (-not (Test-Path (Split-Path $LogPath) -PathType Container)) {   
+        Write-Host "Folder doesn't exist $(Split-Path $LogPath), creating..."
+        New-Item (Split-Path $LogPath) -ItemType Directory | Out-Null
+    }
+
+    #Test if log file exists, if not seed it with a header row
+    If (-not (Test-Path $LogPath)) {   
+        Write-Host "Log file doesn't exist: $($LogPath), creating..."
+        Add-Content -Value '"TimeStamp","Source","Destination","IPV4Address","Status","ResponseTime"' -Path $LogPath
+    }
+
+    #Log collection loop
+    Write-Host "Beginning Ping monitoring of $TestTarget for $Count tries:"
+    While ($Count -gt 0) {   
+        $Ping = Get-WmiObject Win32_PingStatus -Filter "Address = '$TestTarget'" | Select @{Label="TimeStamp";Expression={Get-Date}},@{Label="Source";Expression={ $_.__Server }},@{Label="Destination";Expression={ $_.Address }},IPv4Address,@{Label="Status";Expression={ If ($_.StatusCode -ne 0) {"Failed"} Else {""}}},ResponseTime
+        $Result = $Ping | Select TimeStamp,Source,Destination,IPv4Address,Status,ResponseTime | ConvertTo-Csv -NoTypeInformation
+        $Result[1] | Add-Content -Path $LogPath
+        Write-Host ($Ping | Select TimeStamp,Source,Destination,IPv4Address,Status,ResponseTime | Format-Table -AutoSize | Out-String)
+        $Count --
+        Start-Sleep -Seconds 1
+    }
+}
+
+$JobCount = 1
+$FullPath = ""
+$JobName = ""
+
+ForEach ($Target in $Targets) {
+    $FullPath = $PSScriptRoot + "\log" + $JobCount + ".csv"
+    $JobName = "PingJob" + $JobCount
+    Start-Job -Name $JobName -ScriptBlock $Code -ArgumentList @($Hours, $Target, $FullPath)
+    $JobCount ++
+}
